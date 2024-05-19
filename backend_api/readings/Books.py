fddc355fcb310b9350c91a5db5c7df1ef35d59d7
Brayan Lagos <camilolagos5670@gmail.com>
2024-05-19 01:44:14 -0500

    punto 19

--- backend_api/readings/Books.py
+++ backend_api/readings/Books.py
@@ -14,10 +14,12 @@ class books(models.Model):
     pages = models.IntegerField()
     lenguage = models.CharField(max_length = 30)
     description = models.TextField()
-    over_image_url = models.ImageField()
+    over_image_url = models.ImageField(upload_to='books/', null=True, blank=True)
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
     
     def __str__(self):
-        return self.title
+        return f"{self.title}-{self.author}-{self.genre}-{self.published_year}
+                -{self.isbn}-{self.publisher}-{self.pages}-{self.lenguage}
+                -{self.description}-{self.over_image_url}"
--- backend_api/readings/admin.py
+++ backend_api/readings/admin.py
@@ -3,3 +3,11 @@ from .Books import books
 
 admin.site.register(books)
 # Register your models here.
+
+class booksFields(admin.ModelAdmin):
+    list_display = ('title','author','genre','published_year',
+                    'isbn','publisher','pages','lenguage',
+                    'description','over_image_url')
+    
+    
+admin.site.register(books)
